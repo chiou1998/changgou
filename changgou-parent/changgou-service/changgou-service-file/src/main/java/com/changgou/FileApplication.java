0329862079be1f9c776ccff7813688a3cb552fee
chiou1998 <2601424573@qq.com>
2022-12-07 09:32:31 +0800

    changgou

--- changgou-parent/changgou-service/changgou-service-file/src/main/java/com/changgou/FileApplication.java
+++ changgou-parent/changgou-service/changgou-service-file/src/main/java/com/changgou/FileApplication.java
@@ -2,11 +2,10 @@ package com.changgou;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-//@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableDiscoveryClient
 public class FileApplication {
     public static void main(String[] args) {
--- changgou-parent/changgou-service/changgou-service-goods/src/main/java/com/changgou/goods/service/impl/ActivityServiceImpl.java
+++ changgou-parent/changgou-service/changgou-service-goods/src/main/java/com/changgou/goods/service/impl/ActivityServiceImpl.java
@@ -1,11 +1,11 @@
 package com.changgou.goods.service.impl;
 
 import com.changgou.core.service.impl.CoreServiceImpl;
-//import com.changgou.goods.dao.ActivityMapper;
+import com.changgou.goods.dao.ActivityMapper;
 import com.changgou.goods.dao.ActivityMapper;
 import com.changgou.goods.pojo.Activity;
 import com.changgou.goods.service.ActivityService;
-//import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import tk.mybatis.mapper.common.Mapper;
@@ -20,9 +20,6 @@ public class ActivityServiceImpl extends CoreServiceImpl<Activity> implements Ac
 
     private ActivityMapper activityMapper;
 
-  public ActivityServiceImpl(Mapper<Activity> baseMapper, Class<Activity> clazz) {
-      super(baseMapper, clazz);
-  }
 
     @Autowired
     public ActivityServiceImpl(ActivityMapper activityMapper) {
